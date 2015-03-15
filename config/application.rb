4caa710126aad150a5eb84b7b603c4d59c229ae0
Joshua Mun <joshmun23@gmail.com>
2015-03-15 14:45:03 -0400

    remove Dotenv::Railtie.load from application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,6 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-HOSTNAME = ENV['HOSTNAME']
-
 module LetsMeat
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
