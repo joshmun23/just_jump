9766d90b2c3b578bb87abad81ebb85ea51500e96
Joshua Mun <joshmun23@gmail.com>
2015-04-13 16:32:23 -0400

    added users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'jump#index'
 
+  devise_for :users
+
   resources :jump, only: [:index]
 
   resources :spots, only: [:index, :create, :new]
 
   resources :food_searches
-
 end
