5e703d1c3071a4e5a5ee1b9b5885e278579fc814
Joshua Mun <joshmun23@gmail.com>
2015-04-15 16:14:17 -0400

    added maps, cleaned up formatting

--- app/controllers/food_searches_controller.rb
+++ app/controllers/food_searches_controller.rb
@@ -15,16 +15,22 @@ class FoodSearchesController < ApplicationController
     @spot = @spot.find_or_create_food_search
 
     if @spot.save
-      @restaurant_data = FetchEats.new(food_search_params[:search_term],
-                                       food_search_params[:search_location]).fetch_all_data
+      @restaurant_data = FetchEats.new(
+        food_search_params[:search_term],
+        food_search_params[:search_location]
+      ).fetch_all_data
 
 
-      @food_search_count = FoodSearch.where(food_search_params).first.total_search_count
+      @food_search_count = FoodSearch.where(
+        food_search_params
+      ).first.total_search_count
+
       @food_search_string = "Users have searched #{@food_search_count} time(s) for
                              #{food_search_params[:search_term]} in
                              #{food_search_params[:search_location].capitalize}"
 
-      @most_popular_searches = FoodSearch.where(search_location: food_search_params[:search_location])
+      @most_popular_searches = FoodSearch.where(
+        search_location: food_search_params[:search_location])
       @most_popular_searches = @most_popular_searches.order(total_search_count: :desc)
 
       @spot = FoodSearch.new
