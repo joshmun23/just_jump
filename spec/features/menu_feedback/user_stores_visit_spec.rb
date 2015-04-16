5384084f41293f605bb233c455ac68b5bf9fdce4
Joshua Mun <joshmun23@gmail.com>
2015-04-16 11:20:08 -0400

    configured rspec, capybara, factory girls. also added username to user model

---
+++ spec/features/menu_feedback/user_stores_visit_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'User saves a restaurant and menu item',
+  %{ As a user, I want to be able to store a
+     restaurant and menu item, so that I can
+     review it later for others to see. } do
+
+    # [ ] User searches for menu item and saves the
+    #     info to the Spots Model
+
+  scenario 'user saves spot to review later' do
+
+  end
+end
