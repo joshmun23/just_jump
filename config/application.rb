61d602ae2eb40698df3cd35fa0bc86e4fa4b9cec
Joshua Mun <joshmun23@gmail.com>
2015-04-24 14:59:37 -0400

    add assets configuration for heroku

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module LetsMeat
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
 
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**/}')]
   end
